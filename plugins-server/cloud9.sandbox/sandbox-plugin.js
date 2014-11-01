0669db7b1cdc59f8034f354f763904220ee6545f
Faruk Can KAYA <farukcank@gmail.com>
2014-11-01 13:35:44 +0200

    Working on port redirection.

--- plugins-server/cloud9.sandbox/sandbox-plugin.js
+++ plugins-server/cloud9.sandbox/sandbox-plugin.js
@@ -3,7 +3,38 @@
 var assert = require("assert");
 var netutil = require("netutil");
 var url = require("url");
+var http = require("http");
+function getPortFromRemote(options, callback){
+	var portAssignerURL = url.parse(options.portAssignerURL);
+	if (!portAssignerURL.query)
+		portAssignerURL.query = {};
+	portAssignerURL.query.workspaceId = options.workspaceId;
+	portAssignerURL = url.parse(url.format(portAssignerURL));
+	var reqOptions = {};	
+	reqOptions.hostname = portAssignerURL.host;
+	reqOptions.port = portAssignerURL.port;
+	reqOptions.path = portAssignerURL.path;	
+	
+	var cb = function(response) {
+	  var str = ''
+	  response.on('data', function (chunk) {
+		str += chunk;
+	  });
 
+	  response.on('end', function () {
+		if (response.statusCode!=200){
+			var error = new Error("Response code: " + res.statusCode + " for : "+path);
+			callback(error, 0);
+		} else {
+			callback(null, parseInt(str));
+		}
+		console.log(str);
+	  });
+	};
+
+	var req = http.request(options, cb);
+	req.end();	
+}
 module.exports = function(options, imports, register) {
 
     assert(options.workspaceId, "option 'workspaceId' is required");
@@ -23,18 +54,20 @@ module.exports = function(options, imports, register) {
                 callback(null, options.unixId || null);
             },
             getPort: function(callback) {
-                // grab a free port
-                netutil.findFreePort(20000, 64000, options.host, function (err, port) {
-					console.log("FOUND FREE PORT: "+port+" workspaceId:\""+options.workspaceId+"\" host:\"" +options.host+"\" projectDir:\""+options.projectDir+"\" portassignerurl:\""+options.portAssignerURL+"\"");
-					if (options.portAssignerURL && options.portAssignerURL !== null){
-						var portAssignerURL = url.parse(options.portAssignerURL);
-						if (!portAssignerURL.query)
-							portAssignerURL.query = {};
-						portAssignerURL.query.workspaceId = options.workspaceId;
-						console.log(url.format(portAssignerURL));
-					}
-                    callback(err, port);
-                });
+				console.log("FOUND FREE PORT: "+port+" workspaceId:\""+options.workspaceId+"\" host:\"" +options.host+"\" projectDir:\""+options.projectDir+"\" portassignerurl:\""+options.portAssignerURL+"\"");
+				if (options.portAssignerURL && options.portAssignerURL !== null){
+					getPortFromRemote(options, function(err, port){
+						if (err)
+							console.log(err);
+						console.log(port);
+						callback(err, port);
+					});
+				}else{
+					// grab a free port
+					netutil.findFreePort(20000, 64000, options.host, function (err, port) {					
+						callback(err, port);
+					});
+				}
             },
             getHost: function(callback) {
                 callback(null, options.host);
